7631942a50526d9438582b8caa802c7123822cbb
Erhesvara <106751010+Mahesvaraaaa@users.noreply.github.com>
2023-11-29 20:44:31 +0800

    code

--- word_counter.py
+++ word_counter.py
@@ -10,10 +10,19 @@ for word in split_sentence:
     else:
         word_count1[word] = 1
 
-print(word_count1)
+print(f"User words counter {word_count1}")
 
 # Where there is a word ready
-
 sentence = ("Let us buy a new collection of fish for our project aqua-scape, let's buy something unique fish with "
             "different color. this project will look good and nice with our new fish collection")
 
+word_count2 = {}
+
+own_sentence = sentence.replace(',', '').lower().split()
+for word in own_sentence:
+    if word in word_count2.keys():
+        word_count2[word] += 1
+    else:
+        word_count2[word] = 1
+
+print(f"Ready made sentence word count {word_count2}")
