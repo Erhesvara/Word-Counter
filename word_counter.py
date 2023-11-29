74a863c4aa446a8b5e4e6e20943fbc1d866d719d
Erhesvara <106751010+Mahesvaraaaa@users.noreply.github.com>
2023-11-29 20:37:24 +0800

    code

--- word_counter.py
+++ word_counter.py
@@ -1,12 +1,19 @@
+# Where user will input the words
 user_sentence = input("Enter your words: ")
 
 split_sentence = user_sentence.replace(',', '').lower().split()
-word_count = {}
+word_count1 = {}
 
 for word in split_sentence:
-    if word in word_count.keys():
-        word_count[word] += 1
+    if word in word_count1.keys():
+        word_count1[word] += 1
     else:
-        word_count[word] = 1
+        word_count1[word] = 1
 
-print(word_count)
+print(word_count1)
+
+# Where there is a word ready
+
+sentence = ("Let us buy a new collection of fish for our project aqua-scape, let's buy something unique fish with "
+            "different color. this project will look good and nice with our new fish collection")
+
